221a47ffe49ccf880ce7a8f6c9baee9cb9d442bc
HowlOfTheVoid <ehgraupmann@gmail.com>
2024-06-26 10:29:20 -0600

    Re-Try: Now with C# Class Lib

--- Class1.cs
+++ Class1.cs
@@ -1,6 +1,12 @@
-﻿namespace Arsenal;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace Arsenal
 {
-
+    public class Class1
+    {
+    }
 }
