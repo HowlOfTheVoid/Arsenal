eda0701e5f99959d4554e67168ee807e11195ae8
HowlOfTheVoid <ehgraupmann@gmail.com>
2024-06-26 10:58:58 -0600

    Ignoring References, Made DLL

--- ArsenalMain.cs
+++ ArsenalMain.cs
@@ -13,11 +13,23 @@ using UnityEngine;
 
 namespace Arsenal
 {
-    public class ArsenalMain : BaseUnityPlugin
-    {
-        private void OnEnable()
-        {
+	[BepInDependency("slime-cubed.slugbase", BepInDependency.DependencyFlags.HardDependency)]
 
+	[BepInPlugin("EW.thearsenal", "EW.thearsenal", "0.0.1")]
+	public class ArsenalMain : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "EW.thearsenal";
+        public const string PLUGIN_NAME = "The Arsenal Slugcat";
+        public const string PLUGIN_VERSION = "0.0.1";
+
+		private void OnEnable()
+        {
+            On.Player.Update += PlayerUpdateHook;
+        }
+
+        void PlayerUpdateHook(On.Player.orig_Update orig, Player self, bool eu)
+        {
+            orig(self, eu);
         }
     }
 }
